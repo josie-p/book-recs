c4b366b812a9776e17f0ea9ffd9a62404b54c49e
Josie Parker <josieparker@Josies-MacBook-Pro.local>
2023-05-25 16:02:32 -0500

    still very buggy

--- src/components/Fantasy.jsx
+++ src/components/Fantasy.jsx
@@ -10,34 +10,55 @@ const Fantasy = () => {
     - on submit, pass terms to final page
     */
 
+    let initArray = ["fantasy"]
+
     //subject array:
-    const [subjects, setSubjects] = useState(["fantasy"]);
+    const [subjects, setSubjects] = useState(initArray);
+
+    // setSubjects(subjects.unshift("fantasy"));
 
     //function to add/take away from the array:
     const changeSubjects = (term) => {
 
+        // setSubjects(subjects.unshift("fantasy"));
+
+        console.log(subjects, "what's the subjects array look like at the beginning?")
+
+        console.log(Array.isArray(subjects), "type of subjects");
+
+        if(!Array.isArray(subjects)){
+            console.log("subjects isn't an array babe")
+            setSubjects([initArray]);
+            console.log(subjects, Array.isArray(subjects), "subjects type of subjects");
+        }
+
         // const checkIfExists = subjects.includes("high");
         const checkIfExists = subjects.indexOf(term);
-
-        // subjects.unshift("silly");
-
-        // console.log("just a test", subjects);
 
         if(checkIfExists === -1){
             //add term to the front of the array
             // setSubjects(subjects.push(term));
-            subjects.unshift(term)
+
+            console.log(subjects, "subjects before unshift!");
+
+            // subjects.unshift(term)
+            setSubjects(subjects.unshift(term));
 
             console.log(subjects, "subjects array if array does not include term!");
         }else{
+
+            console.log("getting to the else statement")
+
             //get the index of the term:
             const indexOfTerm = subjects.indexOf(term);
 
             //double check to make sure that the term does exist in the array!
-            if(indexOfTerm !== -1){
-                //take term out of subjects array
-                setSubjects(subjects.splice(indexOfTerm, 1));
+            if(indexOfTerm !== -1 && subjects.includes(term)){
+                // take term out of subjects array
+                console.log(setSubjects(subjects.slice(indexOfTerm, indexOfTerm + 1)), "if statement in else");
+                // setSubjects(subjects.slice(indexOfTerm, indexOfTerm + 1));
             }
+
 
             console.log(subjects, "subjects array if the term did exist in the array!");
 
